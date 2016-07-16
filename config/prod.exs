1b5c02165e1890406d701d8e00c5da8146ba9676
alex_formagio <alexandreformagio14@gmail.com>
2016-07-15 22:26:45 -0300

    ready to heroku2

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
-  force_ssl: [rewrite_on:[:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "quiet-reaches-45347.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
