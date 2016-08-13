a3adc2873727c64682684e0f7f4cb311124613b7
alex_formagio <alexandreformagio14@gmail.com>
2016-08-13 11:14:03 -0300

    update dependencies

--- mix.exs
+++ mix.exs
@@ -31,15 +31,15 @@ defmodule Peepchat.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [{:phoenix, "~> 1.2.0"},
+     #{:poison, "~> 2.2", [hex: :poison, override: true]},
+     #{:ecto, "~> 2.0", [hex: :ecto, override: true]},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
+     {:phoenix_ecto, "~> 3.0.1"},
+     {:postgrex, ">= 0.11.2"},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:poison, "~> 1.5", [hex: :poison, override: true]},
-     {:ecto, "~> 2.0", [hex: :ecto, override: true]},     
      {:cors_plug, "~> 1.1"},
-     {:guardian, "~> 0.10.0"},
+     {:guardian, "~> 0.12.0"},
      {:comeonin, "~> 2.4"},
      {:ja_serializer, "~> 0.10.1"}]
   end
